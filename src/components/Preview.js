04912acff676d555f3a7a0ab6a00f3f19c375b0b
Shashank Shekhar Pandey <shashankspis20@gmail.com>
2024-08-10 04:49:16 +0530

    Initial commit

---
+++ src/components/Preview.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Preview extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="previewContainer">
+                <h2>This is where the preview will go</h2>
+            </div>
+        );
+    }
+}
+
+export default Preview;
