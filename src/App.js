7044d0e44f79133d5367a4f864382d172b2ba3ac
Shashank Shekhar Pandey <shashankspis20@gmail.com>
2024-08-10 04:28:07 +0530

    Import Header and Main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 //import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 class App extends Component {
     constructor() {
@@ -7,7 +9,12 @@ class App extends Component {
     }
 
     render() {
-        return <div className="App"></div>;
+        return (
+            <div className="App">
+                <Header />
+                <Main />
+            </div>
+        );
     }
 }
 
