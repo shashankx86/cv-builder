0364a1d9bf0c77bfd4d11305b984f1a9b78d9885
Shashank Shekhar Pandey <shashankspis20@gmail.com>
2024-08-10 07:04:38 +0530

    Add index

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,54 +6,55 @@ class Education extends Component {
     }
 
     render() {
+        const { index, deleteComponent } = this.props;
         return (
             <div className="education">
                 <input
                     type={"text"}
-                    id="uni"
+                    id={`uni${index}`}
                     name="uni"
                     placeholder="University Name"
                 ></input>
                 <input
                     type={"text"}
-                    id="uniCity"
+                    id={`uniCity${index}`}
                     name="uniCity"
                     placeholder="City"
                 ></input>
                 <input
                     type={"text"}
-                    id="degree"
+                    id={`degree${index}`}
                     name="degree"
                     placeholder="Degree"
                 ></input>
                 <input
                     type={"text"}
-                    id="subject"
+                    id={`subject${index}`}
                     name="subject"
                     placeholder="Subject"
                 ></input>
                 <input
                     type={"date"}
-                    id="uniFrom"
+                    id={`uniFrom${index}`}
                     name="uniFrom"
                     placeholder="From"
                 ></input>
                 <input
                     type={"date"}
-                    id="uniTo"
+                    id={`uniTo${index}`}
                     name="uniTo"
                     placeholder="To"
                 ></input>
                 <label htmlFor="uniPresent">I currently go here</label>
                 <input
                     type={"checkbox"}
-                    id="uniPresent"
+                    id={`uniPresent${index}`}
                     name="uniPresent"
                 ></input>
                 <button
                     onClick={(e) => {
                         e.preventDefault();
-                        this.props.deleteComponent();
+                        deleteComponent();
                     }}
                 >
                     Delete
