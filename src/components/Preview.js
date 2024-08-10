886ed6959df8da8ce80d98af0b42bf3e6cea3e35
Shashank Shekhar Pandey <shashankspis20@gmail.com>
2024-08-10 07:49:34 +0530

    Add components to populate CV

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,14 +1,110 @@
 import React, { Component } from "react";
 
-class Preview extends Component {
-    constructor() {
-        super();
+class PopulatePersonal extends Component {
+    constructor(props) {
+        super(props);
     }
 
     render() {
+        const { personalInfo } = this.props;
+        return (
+            <div className="personalContainer">
+                <div className="personalSection">
+                    <div>
+                        <p className="name">{personalInfo.content.name}</p>
+                        <p className="title">{personalInfo.content.title}</p>
+                    </div>
+                    <div className="contactInfo">
+                        <p className="tel">{personalInfo.content.tel}</p>
+                        <p className="email">{personalInfo.content.email}</p>
+                        <p className="web">{personalInfo.content.web}</p>
+                        <p className="address">
+                            {personalInfo.content.address}
+                        </p>
+                    </div>
+                </div>
+                <div className="descriptionSection">
+                    {personalInfo.content.description}
+                </div>
+            </div>
+        );
+    }
+}
+
+class PopulateEducation extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { educationInfoList } = this.props;
+        let educationSections = [];
+        educationInfoList.forEach((element) => {
+            let section = (
+                <div className="educationSection">
+                    <div>
+                        <p className="degree">{element.content.degree}</p>
+                        <p className="uni">{element.content.uni}</p>
+                    </div>
+                    <div>
+                        <p className="uniCity">{element.content.uniCity}</p>
+                    </div>
+                </div>
+            );
+
+            educationSections.push(section);
+        });
+
+        return <div className="educationSections">{educationSections}</div>;
+    }
+}
+
+class PopulateWork extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { workInfoList } = this.props;
+        let workSections = [];
+        workInfoList.forEach((element) => {
+            let section = (
+                <div className="workSection">
+                    <div>
+                        <p className="position">{element.content.position}</p>
+                        <p className="company">{element.content.company}</p>
+                    </div>
+                    <div>
+                        <p className="workCity">{element.content.workCity}</p>
+                    </div>
+                </div>
+            );
+
+            workSections.push(section);
+        });
+
+        return <div className="workSections">{workSections}</div>;
+    }
+}
+
+class Preview extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { personalInfo, educationInfoList, workInfoList } = this.props;
         return (
             <div className="previewContainer">
-                <h2>This is where the preview will go</h2>
+                <PopulatePersonal personalInfo={personalInfo} />
+                <div className="educationContainer">
+                    <h3>Education</h3>
+                    <PopulateEducation educationInfoList={educationInfoList} />
+                </div>
+                <div className="workContainer">
+                    <h3>Work Experience</h3>
+                    <PopulateWork workInfoList={workInfoList} />
+                </div>
             </div>
         );
     }
